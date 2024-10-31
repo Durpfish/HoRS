a7e14ce7f5acca3f186edbec83903f22b6748519
REPLACE_WITH_YOUR_NAME <REPLACE_WITH_YOUR_EMAIL>
2024-11-01 00:12:16 +0800

    verion 2 - edited roomalloc, customer and partner entities

--- HotelReservationSystemLibrary/src/entity/Guest.java
+++ HotelReservationSystemLibrary/src/entity/Guest.java
@@ -24,25 +24,25 @@ public class Guest implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long guestId;
     
-    @Column(nullable = false)
+    @Column(length = 64, nullable = false)
     private String firstName;
 
-    @Column(nullable = false)
+    @Column(length = 64, nullable = false)
     private String lastName;
 
-    @Column(nullable = false, unique = true)
+    @Column(length = 64, nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
     private String phoneNumber;
 
-    @Column(nullable = false, unique = true)
+    @Column(length = 64, nullable = false, unique = true)
     private String passportNumber; 
 
-    @Column(nullable = false, unique = true)
+    @Column(length = 64, nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false)
+    @Column(length = 64, nullable = false)
     private String password;
 
     public Long getGuestId() {
