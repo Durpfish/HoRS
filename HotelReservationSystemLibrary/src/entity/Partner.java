d68d3c056ac23d45b85faacaa4352f3e1d836728
REPLACE_WITH_YOUR_NAME <REPLACE_WITH_YOUR_EMAIL>
2024-11-01 00:11:57 +0800

    verion 2 - edited roomalloc, customer and partner entities

--- HotelReservationSystemLibrary/src/entity/Partner.java
+++ HotelReservationSystemLibrary/src/entity/Partner.java
@@ -5,6 +5,7 @@
 package entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +22,24 @@ public class Partner implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long partnerId;
+    
+    @Column(length = 64, nullable = false)
+    private String name;
+
+    @Column(length = 64)
+    private String contactPerson;
+
+    @Column(length = 64, unique = true)
+    private String email;
+
+    @Column(length = 20)
+    private String phone;
+
+    @Column(length = 64, nullable = false, unique = true)
+    private String username;
+
+    @Column(length = 64, nullable = false)
+    private String password;
 
     public Long getPartnerId() {
         return partnerId;
@@ -28,6 +47,54 @@ public class Partner implements Serializable {
 
     public void setPartnerId(Long partnerId) {
         this.partnerId = partnerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
